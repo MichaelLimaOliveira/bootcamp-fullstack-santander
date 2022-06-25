0d1dfb3d9fa84a5de2472089853cc0407e64067e
Michael <michaellima.contato@gmail.com>
2022-06-24 22:53:57 -0300

    atividades DOM BOM

---
+++ Módulo II - HTML_CSS3_JS/DOM e BOM/exemplo-dom.js
@@ -0,0 +1,8 @@
+const title = document.getElementById("#title")
+
+const arrayLi = document.getElementsByTagName('li')
+
+document.queryselectorAll(".primeira-class .segunda-class")
+// retorna todos os elementos em array que tenha essas duas classes
+document.queryselectorAll("li .opcao")
+// ou ate combinar tags e classes

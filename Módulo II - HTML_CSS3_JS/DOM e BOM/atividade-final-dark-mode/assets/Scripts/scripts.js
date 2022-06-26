1dd5942d8818c9dd8bb135c1ca2cc27ec37c9094
Michael <michaellima.contato@gmail.com>
2022-06-25 21:06:59 -0300

    Finalizado modulo de DOM e BOM

--- Módulo II - HTML_CSS3_JS/DOM e BOM/atividade-final-dark-mode/assets/Scripts/scripts.js
+++ Módulo II - HTML_CSS3_JS/DOM e BOM/atividade-final-dark-mode/assets/Scripts/scripts.js
@@ -2,23 +2,27 @@ const bodyPage = document.getElementsByTagName('body')[0]
 const titlelH1 = document.getElementById('page-title')
 const button = document.getElementById('mode-selector')
 const footer = document.getElementById('footer-site')
+const darkModeClass = 'dark-mode'
 
-function changePageMode() {
-    bodyPage.classList.toggle('dark-mode')
-    footer.classList.toggle('dark-mode')
-    button.classList.toggle('dark-mode')
-    titlelH1.classList.toggle('dark-mode')
-
-    changeButtunContent();
-    changeTitleContent();
+function changeClasses() {
+    bodyPage.classList.toggle(darkModeClass)
+    footer.classList.toggle(darkModeClass)
+    button.classList.toggle(darkModeClass)
+    titlelH1.classList.toggle(darkModeClass)
 }
 
 function changeButtunContent() {
-    (button.className === 'dark-mode') ? button.innerHTML = 'Light Mode' : button.innerHTML = 'Dark Mode';
+    (button.className === darkModeClass) ? button.innerHTML = 'Light Mode' : button.innerHTML = 'Dark Mode';
 }
 
 function changeTitleContent() {
-    (titlelH1.className === 'dark-mode') ? titlelH1.innerHTML = 'Dark Mode ON' : titlelH1.innerHTML = 'Light Mode ON';
+    (titlelH1.classList.contains(darkModeClass)) ? titlelH1.innerHTML = 'Dark Mode ON' : titlelH1.innerHTML = 'Light Mode ON';
+}
+
+function changePageMode() {
+    changeClasses();
+    changeButtunContent();
+    changeTitleContent();
 }
 
 button.addEventListener('click', changePageMode) 

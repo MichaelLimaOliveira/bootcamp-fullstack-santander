479a00a97da1e4d152459739dc2686da744a943e
Michael Notebook <michaellima.contato@gmail.com>
2022-07-02 15:35:05 -0300

    primeiras mudanças

---
+++ Módulo III - Angular/course-manager/src/app/course/course-list.components.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from "@angular/core";
+import { Course } from "./course";
+
+@Component({
+    selector: 'app-course-list',
+    templateUrl: './course-list.components.html'
+})
+
+export class CourseListComponent  implements OnInit {
+
+    courses: Course[] = [];
+
+    ngOnInit(): void {
+        this.courses = [
+            {
+                id: 1,
+                name: 'Angular: Forms',
+                imageUrl: '',
+                price: '99.99',
+                code: 'XPS-8796',
+                duration: 120,
+                rating: 5.4
+            }
+        ]
+    }
+}
---
+++ Módulo III - Angular/course-manager/src/app/course/course.ts
@@ -0,0 +1,9 @@
+export class Course {
+    id: number = 0;
+    name: string = '';
+    imageUrl: string = '';
+    price: string = '';
+    code: string = '';
+    duration: number = 0;
+    rating: number = 0;
+}

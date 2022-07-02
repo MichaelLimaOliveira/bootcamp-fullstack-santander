6a4627fdedd7be3639f4d10eb7d30edad4281623
Michael <michaellima.contato@gmail.com>
2022-07-02 00:01:18 -0300

    primeira aula angular

--- Módulo II - HTML_CSS3_JS/TypeScript/tuplas.js
+++ Módulo II - HTML_CSS3_JS/TypeScript/tuplas.js
@@ -3,5 +3,32 @@ let pessoa;
 pessoa = ['Michael lima', 'Estudante', 28];
 console.log(pessoa);
 console.log(pessoa[1]);
-let listaFrutas = ['maca', 'pera', 'uva', 'banana'];
+let listaFrutas = ['maca', 'pera', 'uva', 'banana', 'kaki', 'kiwi', 'laranja', 'tomate'];
 console.log(...listaFrutas);
+let pessoa2 = ['Michael lima', 'Estudante', 28];
+let listaFrutas2 = [5, true, ...listaFrutas];
+console.log(listaFrutas2);
+function listaDePessoas(nomes, idades) {
+    return [...nomes, ...idades];
+}
+console.log(listaDePessoas(['esteevao', 'alexandre', 'caio', 'augusto', 'antonio', 'michael'], [22, 22, 22, 22, 22, 28]));
+function criarPessoa(...nome) {
+    return [...nome];
+}
+console.log(criarPessoa('michael', 'lima', 'oliveira'));
+var Idioma;
+(function (Idioma) {
+    Idioma[Idioma["Prtugues"] = 0] = "Prtugues";
+    Idioma[Idioma["Ingles"] = 1] = "Ingles";
+    Idioma[Idioma["Espanhol"] = 2] = "Espanhol";
+    Idioma[Idioma["Frances"] = 3] = "Frances";
+})(Idioma || (Idioma = {}));
+var Idioma2;
+(function (Idioma2) {
+    Idioma2["Prtugues"] = "Pt-Br";
+    Idioma2["Ingles"] = "En";
+    Idioma2["Espanhol"] = "Es";
+    Idioma2["Frances"] = "Fr";
+})(Idioma2 || (Idioma2 = {}));
+console.log(Idioma);
+console.log(Idioma2.Prtugues);
--- Módulo II - HTML_CSS3_JS/TypeScript/tuplas.ts
+++ Módulo II - HTML_CSS3_JS/TypeScript/tuplas.ts
@@ -4,6 +4,46 @@ pessoa = ['Michael lima', 'Estudante', 28]
 console.log(pessoa);
 console.log(pessoa[1]);
 
-let listaFrutas: [string, ...string[]] = ['maca', 'pera', 'uva', 'banana']
+let listaFrutas: [string, ...string[]] = ['maca', 'pera', 'uva', 'banana', 'kaki', 'kiwi', 'laranja', 'tomate']
 console.log(...listaFrutas);
 
+let pessoa2: [name: string, ocupation: string, age: number] = ['Michael lima', 'Estudante', 28];
+
+let listaFrutas2: [number, boolean, ...string[]] = [5, true, ...listaFrutas];
+
+console.log(listaFrutas2)
+
+function listaDePessoas(nomes: string[], idades: number[]) {
+    return [...nomes, ...idades]
+}
+
+console.log(listaDePessoas(['esteevao', 'alexandre', 'caio', 'augusto', 'antonio', 'michael'], [22, 22, 22, 22, 22, 28]));
+
+type Nome = 
+        | [primeiroNome: string, sobrenome: string]
+        | [primeiroNome: string, nomeDoMeio: string, ultimoNome: string]
+        | [age: number, name: string, ocupation: boolean]
+
+function criarPessoa(...nome: Nome) {
+    return [...nome]
+}
+
+console.log(criarPessoa('michael', 'lima', 'oliveira'))
+
+enum Idioma {
+    Prtugues,
+    Ingles,
+    Espanhol,
+    Frances
+}
+
+
+enum Idioma2 {
+    Prtugues = 'Pt-Br',
+    Ingles = 'En',
+    Espanhol = 'Es',
+    Frances = 'Fr'
+}
+
+console.log(Idioma)
+console.log(Idioma2.Prtugues)

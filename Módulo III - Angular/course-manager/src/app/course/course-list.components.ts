14177680d46a5e533957daaabefe6c3c35195c07
Michael Notebook <michaellima.contato@gmail.com>
2022-07-02 21:40:14 -0300

    adicionando cursos e estruturando pagina com injeçao de dependencia

--- Módulo III - Angular/course-manager/src/app/course/course-list.components.ts
+++ Módulo III - Angular/course-manager/src/app/course/course-list.components.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { Course } from "./course";
+import { CourseService } from "./course.service";
 
 @Component({
     selector: 'app-course-list',
@@ -10,17 +11,8 @@ export class CourseListComponent  implements OnInit {
 
     courses: Course[] = [];
 
+    constructor(private courseService: CourseService) {}
     ngOnInit(): void {
-        this.courses = [
-            {
-                id: 1,
-                name: 'Angular: Forms',
-                imageUrl: '',
-                price: '99.99',
-                code: 'XPS-8796',
-                duration: 120,
-                rating: 5.4
-            }
-        ]
+        this.courses = this.courseService.retriveAll(); 
     }
 }

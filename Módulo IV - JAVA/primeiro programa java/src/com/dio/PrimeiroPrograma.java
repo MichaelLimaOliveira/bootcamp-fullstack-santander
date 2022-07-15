2815f135b825095c44e854fceba1c5aed0c4b096
Michael Notebook <michaellima.contato@gmail.com>
2022-07-15 16:44:53 -0300

    consertei o git

--- Módulo IV - JAVA/primeiro programa java/src/com/dio/PrimeiroPrograma.java
+++ Módulo IV - JAVA/primeiro programa java/src/com/dio/PrimeiroPrograma.java
@@ -11,9 +11,9 @@ public class PrimeiroPrograma {
 		
 		System.out.println(gato);
 		
-		/*int a = 2;
+		int a = 2;
 		int b = 3;
-		System.out.println("Hello Wordl!" + (a + b));*/
+		System.out.println("Hello Wordl!" + (a + b));
 
 	}
 

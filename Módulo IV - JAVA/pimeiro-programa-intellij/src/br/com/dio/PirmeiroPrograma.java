7f252460b366af50ab8450ec6b02f62467a08e11
Michael Notebook <michaellima.contato@gmail.com>
2022-07-16 15:06:38 -0300

    classe cal culadora

--- Módulo IV - JAVA/pimeiro-programa-intellij/src/br/com/dio/PirmeiroPrograma.java
+++ Módulo IV - JAVA/pimeiro-programa-intellij/src/br/com/dio/PirmeiroPrograma.java
@@ -3,6 +3,8 @@ package br.com.dio;
 
 import br.com.dio.model.Gato;
 
+import javax.xml.namespace.QName;
+
 public class PirmeiroPrograma {
     public static void main(String[] args) {
         /*int a = 5;
@@ -11,5 +13,54 @@ public class PirmeiroPrograma {
 
         Gato gato = new Gato();
         System.out.println(gato);
+
+        Magazines magazine = new Magazines("Cazaki", "300", 200);
+
+        System.out.println(magazine);
+    }
+}
+
+class Magazines {
+    private String name;
+    private String pages;
+    private Number value;
+
+    public Magazines(String name, String pages, Number value) {
+        this.name = name;
+        this.pages = pages;
+        this.value = value;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPages() {
+        return pages;
+    }
+
+    public void setPages(String pages) {
+        this.pages = pages;
+    }
+
+    public Number getValue() {
+        return value;
+    }
+
+    public void setValue(Number value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return "Magazines{" +
+                "name='" + name + '\'' +
+                ", pages='" + pages + '\'' +
+                ", value=" + value +
+                '}';
     }
 }

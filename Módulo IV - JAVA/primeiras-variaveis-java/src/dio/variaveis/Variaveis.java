fbb1ea525fb313d91cf0f84e80412dde6d317cd6
Michael <michaellima.contato@gmail.com>
2022-07-16 18:58:05 -0300

    primeira aula de variaveis java

---
+++ Módulo IV - JAVA/primeiras-variaveis-java/src/dio/variaveis/Variaveis.java
@@ -0,0 +1,16 @@
+package dio.variaveis;
+
+import javax.management.ValueExp;
+
+public class Variaveis {
+    public static void main(String[] args) {
+        final int VALUE = 1;
+        int money = 22;
+        int quantityMoney = 30;
+
+        System.out.println(VALUE);
+        System.out.println(money);
+        System.out.println(quantityMoney);
+
+    }
+}

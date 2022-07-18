2b3e120174c957f65020baf9877ed91ebf4368fb
Michael <michaellima.contato@gmail.com>
2022-07-18 02:33:45 -0300

    atividades

---
+++ Módulo IV - JAVA/atividades-modulo-metodos/src/atividades/controladoresDeFluxo/main.java
@@ -0,0 +1,23 @@
+package atividades.controladoresDeFluxo;
+
+public class main {
+    public static void main(String[] args) {
+
+        int i1 = 10;
+        int i2 = 20;
+
+        double d1 = 2.5;
+        double d2 = 3.4;
+
+        float f1 = 4.9f;
+        float f2 = 2.4f;
+
+        String s1 = "aksjdlkançflanfl";
+        String s2 = "asdljkhasldkhalsdçh";
+
+        boolean b1 = false;
+        boolean b2 = true;
+
+        boolean teste = i1 < i2;
+    }
+}

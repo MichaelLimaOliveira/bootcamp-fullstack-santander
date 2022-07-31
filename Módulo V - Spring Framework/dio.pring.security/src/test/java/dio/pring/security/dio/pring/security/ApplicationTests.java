0ad524966e2c2c4b311c9747819662bc5d49db98
Michael Lima <michaellima.contato@gmail.com>
2022-07-31 20:56:04 -0300

    java jwt

---
+++ Módulo V - Spring Framework/dio.pring.security/src/test/java/dio/pring/security/dio/pring/security/ApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.pring.security.dio.pring.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
